d69d852f6a9ec69669a50943db89e10af2e6c164
Uliana Atanasova <kuv25ild@gmail.com>
2023-09-17 21:56:45 +0300

    added hash router

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -1,6 +1,6 @@
 export const getPartOfAdress = (address = "") => address.split(",");
 
-export const getPartsOfCOnditions = (conditions) => conditions.split("\n");
+export const getPartsOfCOnditions = (conditions = "") => conditions.split("\n");
 
 export const checkIsFavorite = (id, favoriteCars) =>
   favoriteCars.find((car) => car.id === id);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import { App } from "./app/App";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -13,9 +13,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>

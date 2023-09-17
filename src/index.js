6fa5082748dbe7dedd7a366e4f83ffd7b062c8ac
Uliana Atanasova <kuv25ild@gmail.com>
2023-09-17 21:45:13 +0300

    test

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/car-advert">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
